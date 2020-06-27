cabc8a628ada4df42f66b9e2a55876c737dbbad4
Riceair <handy00095@gmail.com>
2020-06-27 18:23:52 +0800

    dfads

--- public/javascripts/ProjectShow.js
+++ public/javascripts/ProjectShow.js
@@ -2,59 +2,39 @@ class ProjectShow{
     //待處理事項: 提供下載檔案 上傳檔案資料設定
     constructor(info,imgSrc) {
         this.init=this.init.bind(this) //綁定function
-        this.toModify=this.toModify.bind(this)
+        this.titleDesMod=this.titleDesMod.bind(this)
+        this.updateMod=this.updateMod.bind(this)
         this.toUpdate=this.toUpdate.bind(this)
         this.cancelMod=this.cancelMod.bind(this)
         this.fileChoose=this.fileChoose.bind(this)
         this.fileDel=this.fileDel.bind(this)
         const modifyButton=document.querySelector('.modify')
         this.modifyButton=modifyButton //修改資料按鈕
-        const backButton=document.querySelector('.back')
-        this.backButton=backButton //返回按鈕
-        const titleText=document.querySelector('.titleText')
-        this.titleText=titleText //標題
-        const desText=document.querySelector('.desText')
-        this.desText=desText //描述
-        const posterName=document.querySelector('.posterName')
-        this.posterName=posterName//海報名稱
-        const posterImg=document.querySelector('.posterImg')
-        this.posterImg=posterImg//海報圖片
-        const pptName=document.querySelector('.pptName')
-        this.pptName=pptName//簡報名稱
-        const pptImg=document.querySelector('.pptImg')
-        this.pptImg=pptImg//簡報圖片
-        const docName=document.querySelector('.docName')
-        this.docName=docName//文件名稱
-        const docImg=document.querySelector('.docImg')
-        this.docImg=docImg//文件圖片
-        const codeName=document.querySelector('.codeName')
-        this.codeName=codeName//程式碼名稱
-        const codeImg=document.querySelector('.codeImg')
-        this.codeImg=codeImg//程式碼圖片
-        const titleInput=document.querySelector('.titleInput')
-        this.titleInput=titleInput//標題輸入框
-        const desInput=document.querySelector('.desInput')
-        this.desInput=desInput//描述輸入框
-        const ppdcText=document.querySelector('.ppdcText')
-        this.ppdcText=ppdcText//指引文字
-        const updateButton=document.querySelector('.updateButton')
-        this.updateButton=updateButton//上傳刪除按鈕
-        const posterCh=document.getElementById('posterCh')
-        this.posterCh=posterCh//選擇檔案按鈕
-        const pptCh=document.getElementById('pptCh')
-        this.pptCh=pptCh//選擇投影片按鈕
-        const docCh=document.getElementById('docCh')
-        this.docCh=docCh//選擇文件按鈕
-        const codeCh=document.getElementById('codeCh')
-        this.codeCh=codeCh//程式碼選擇按鈕
-        const posterDel=document.getElementById('posterDel')
-        this.posterDel=posterDel//海報刪除按鈕
-        const pptDel=document.getElementById('pptDel')
-        this.pptDel=pptDel//投影片刪除按鈕
-        const docDel=document.getElementById('docDel')
-        this.docDel=docDel//文件刪除按鈕
-        const codeDel=document.getElementById('codeDel')
-        this.codeDel=codeDel//程式碼刪除按鈕
+        this.modifyTD=document.getElementById("modifyTD")//選擇修改標題/描述
+        this.modifyUpData=document.getElementById("modifyUpData")//選擇上傳檔案
+        this.backButton=document.querySelector('.back') //返回按鈕
+        this.titleText=document.querySelector('.titleText') //標題
+        this.desText=document.querySelector('.desText') //描述
+        this.posterName=document.querySelector('.posterName') //海報名稱
+        this.posterImg=document.querySelector('.posterImg') //海報圖片
+        this.pptName=document.querySelector('.pptName') //簡報名稱
+        this.pptImg=document.querySelector('.pptImg') //簡報圖片
+        this.docName=document.querySelector('.docName') //文件名稱
+        this.docImg=document.querySelector('.docImg') //文件圖片
+        this.codeName=document.querySelector('.codeName') //程式碼名稱
+        this.codeImg=document.querySelector('.codeImg') //程式碼圖片
+        this.titleInput=document.querySelector('.titleInput') //標題輸入框
+        this.desInput=document.querySelector('.desInput') //描述輸入框
+        this.ppdcText=document.querySelector('.ppdcText')//文件指引文字
+        this.updateButton=document.querySelector('.updateButton') //上傳刪除按鈕
+        this.posterCh=document.getElementById('posterCh') //選擇檔案按鈕
+        this.pptCh=document.getElementById('pptCh') //選擇投影片按鈕
+        this.docCh=document.getElementById('docCh') //選擇文件按鈕
+        this.codeCh=document.getElementById('codeCh') //程式碼選擇按鈕
+        this.posterDel=document.getElementById('posterDel') //海報刪除按鈕
+        this.pptDel=document.getElementById('pptDel') //投影片刪除按鈕
+        this.docDel=document.getElementById('docDel') //文件刪除按鈕
+        this.codeDel=document.getElementById('codeDel') //程式碼刪除按鈕
         this.poster=document.querySelector('.poster')
         this.ppt=document.querySelector('.ppt')
         this.doc=document.querySelector('.doc')
@@ -68,7 +48,8 @@ class ProjectShow{
 
     init(certification){
         if(certification){ //專題修改認證
-            this.modifyButton.addEventListener('click',this.toModify)
+            this.modifyTD.addEventListener('click',this.titleDesMod)
+            this.modifyUpData.addEventListener('click',this.updateMod)
         }
         else{
             this.modifyButton.classList.add('hidden')
@@ -88,57 +69,57 @@ class ProjectShow{
         if(info.poster!=null){ //四個檔案初始化
             this.posterName.innerText=info.poster/////////////info.poster.files[0].name
             this.posterImg.setAttribute("src",imgSrc.poster)
-            //設置download
-            $(".poster").click(function(){
-                window.open("http://elearning.nuk.edu.tw/m_teacher/m_tea_txbook_files.php?jteabook_sd=126217")
-            })
+            //設置download css
             this.poster.classList.add('downhover')
-        }
+            this.poster.setAttribute("onclick","posterDownload.click()")
+        }else
+            this.poster.setAttribute("onclick",null)
         if(info.ppt!=null){
             this.pptName.innerText=info.ppt////////////////// info.ppt.files[0].name
             this.pptImg.setAttribute("src",imgSrc.ppt)
-            //設置download
-            $(".ppt").click(function(){
-                window.open("http://elearning.nuk.edu.tw/m_teacher/m_tea_txbook_files.php?jteabook_sd=126217")
-            })
+            //設置download css
             this.ppt.classList.add('downhover')
-        }
+            this.ppt.setAttribute("onclick","pptDownload.click()")
+        }else
+            this.ppt.setAttribute("onclick",null)
         if(info.doc!=null){
             this.docName.innerText=info.doc////////////////// info.doc.files[0].name
             this.docImg.setAttribute("src",imgSrc.doc)
-            //設置download
-            $(".doc").click(function(){
-                window.open("http://elearning.nuk.edu.tw/m_teacher/m_tea_txbook_files.php?jteabook_sd=126217")
-            })
+            //設置download css
             this.doc.classList.add('downhover')
-        }
+            this.doc.setAttribute("onclick","docDownload.click()")
+        }else
+            this.doc.setAttribute("onclick",null)
         if(info.code!=null){
             this.codeName.innerText=info.code///////////////// info.code.files[0].name
             this.codeImg.setAttribute("src",imgSrc.code)
-            //設置download
-            $(".code").click(function(){
-                window.open("http://elearning.nuk.edu.tw/m_teacher/m_tea_txbook_files.php?jteabook_sd=126217")
-            })
+            //設置download css
             this.code.classList.add('downhover')
-        }
+            this.code.setAttribute("onclick","codeDownload.click()")
+        }else
+            this.code.setAttribute("onclick",null)
 
         this.upData={'title':[true,null],'description':[true,null],'poster':[false,null],
         'ppt':[false,null],'doc':[false,null],'code':[false,null]} //Boolean為false表示未經過修改
     }
 
-    toModify(){ //選擇修改觸發
+    titleDesMod(){
+        this.ppdcText.classList.add('hidden')
+        this.updateButton.classList.remove('hidden')//選擇資料按鈕
+
         this.titleText.innerText=""
         this.titleInput.classList.remove('hidden') //標題輸入框
         this.desText.innerText=""
         this.desInput.classList.remove('hidden') //描述輸入框
 
-        this.ppdcText.classList.add('hidden')
-        this.updateButton.classList.remove('hidden')//選擇資料按鈕
-
-        this.posterCh.addEventListener('change',this.fileChoose)//選擇資料按鈕綁定event
-        this.pptCh.addEventListener('change',this.fileChoose)
-        this.docCh.addEventListener('change',this.fileChoose)
-        this.codeCh.addEventListener('change',this.fileChoose)
+        const chooseButton=document.querySelectorAll(".choose")
+        for(let choose of chooseButton){
+            choose.classList.add("hidden")
+        }
+        const deleteButton=document.querySelectorAll(".delete")
+        for(let del of deleteButton){
+            del.classList.remove("hidden")
+        }
 
         this.posterDel.addEventListener('click',this.fileDel)//刪除資料按鈕綁定event
         this.pptDel.addEventListener('click',this.fileDel)
@@ -147,6 +128,31 @@ class ProjectShow{
 
         this.modifyButton.textContent="儲存修改"//"選擇修改"按鈕改成"儲存修改"並綁定event
         this.modifyButton.addEventListener('click',this.toUpdate)
+        this.modifyButton.setAttribute("data-target",null)
+
+        this.backButton.addEventListener('click',this.cancelMod)//"取消"綁定為取消修改(復原)
+    }
+
+    updateMod(){
+        const pT=document.querySelector(".ppdcText")
+        pT.classList.add('hidden')
+        this.updateButton.classList.remove('hidden')//選擇資料按鈕
+
+        const chooseButton=document.querySelectorAll(".choose")
+        for(let choose of chooseButton){
+            choose.classList.remove("hidden")
+        }
+        const deleteButton=document.querySelectorAll(".delete")
+        for(let del of deleteButton){
+            del.classList.add("hidden")
+        }
+
+        this.posterCh.addEventListener('change',this.fileChoose)//選擇資料按鈕綁定event
+        this.pptCh.addEventListener('change',this.fileChoose)
+        this.docCh.addEventListener('change',this.fileChoose)
+        this.codeCh.addEventListener('change',this.fileChoose)
+
+        this.modifyButton.classList.add("hidden")
 
         this.backButton.addEventListener('click',this.cancelMod)//"取消"綁定為取消修改(復原)
     }
@@ -155,6 +161,8 @@ class ProjectShow{
         this.init(info.certification)
         this.modifyButton.textContent="修改資料"
         this.modifyButton.removeEventListener('click',this.toUpdate)
+        this.modifyButton.setAttribute("data-target","#exampleModalCenter")
+        this.modifyButton.classList.remove("hidden")
         this.titleInput.classList.add('hidden')
         this.desInput.classList.add('hidden')
 
@@ -169,7 +177,7 @@ class ProjectShow{
                 this.posterName.innerText=filename
                 this.posterImg.setAttribute("src",imgSrc.poster)
                 this.poster.classList.remove('downhover') //移除下載綁定
-                $(".poster").unbind('click')
+                this.poster.setAttribute("onclick",null)
                 this.upData.poster[0]=true
                 this.upData.poster[1]=this.posterCh.files
                 break
@@ -177,7 +185,7 @@ class ProjectShow{
                 this.pptName.innerText=filename
                 this.pptImg.setAttribute("src",imgSrc.ppt)
                 this.ppt.classList.remove('downhover') //移除下載綁定
-                $(".ppt").unbind('click')
+                this.ppt.setAttribute("onclick",null)
                 this.upData.ppt[0]=true
                 this.upData.ppt[1]=this.pptCh.files
                 break
@@ -185,7 +193,7 @@ class ProjectShow{
                 this.docName.innerText=filename
                 this.docImg.setAttribute("src",imgSrc.doc)
                 this.doc.classList.remove('downhover')  //移除下載綁定
-                $(".doc").unbind('click')
+                this.doc.setAttribute("onclick",null)
                 this.upData.doc[0]=true
                 this.upData.doc[1]=this.docCh.files
                 break
@@ -193,7 +201,7 @@ class ProjectShow{
                 this.codeName.innerText=filename
                 this.codeImg.setAttribute("src",imgSrc.code)
                 this.code.classList.remove('downhover') //移除下載綁定
-                $(".code").unbind('click')
+                this.code.setAttribute("onclick",null)
                 this.upData.code[0]=true
                 this.upData.code[1]=this.codeCh.files
                 break
@@ -206,7 +214,7 @@ class ProjectShow{
                 this.posterName.innerText="尚未上傳檔案"
                 this.posterImg.setAttribute("src","")
                 this.poster.classList.remove('downhover') //移除下載綁定
-                $(".poster").unbind('click')
+                this.poster.setAttribute("onclick",null)
                 this.upData.poster[0]=true
                 this.upData.poster[1]=null
                 break
@@ -214,7 +222,7 @@ class ProjectShow{
                 this.pptName.innerText="尚未上傳檔案"
                 this.pptImg.setAttribute("src","")
                 this.ppt.classList.remove('downhover') //移除下載綁定
-                $(".ppt").unbind('click')
+                this.ppt.setAttribute("onclick",null)
                 this.upData.ppt[0]=true
                 this.upData.ppt[1]=null
                 break
@@ -222,7 +230,7 @@ class ProjectShow{
                 this.docName.innerText="尚未上傳檔案"
                 this.docImg.setAttribute("src","")
                 this.doc.classList.remove('downhover')  //移除下載綁定
-                $(".doc").unbind('click')
+                this.doc.setAttribute("onclick",null)
                 this.upData.doc[0]=true
                 this.upData.doc[1]=null
                 break
@@ -230,7 +238,7 @@ class ProjectShow{
                 this.codeName.innerText="尚未上傳檔案"
                 this.codeImg.setAttribute("src","")
                 this.code.classList.remove('downhover') //移除下載綁定
-                $(".code").unbind('click')
+                this.code.setAttribute("onclick",null)
                 this.upData.code[0]=true
                 this.upData.code[1]=null
                 break
@@ -244,36 +252,35 @@ class ProjectShow{
         let sendData={'title':this.upData['title'][1],'description':this.upData['description'][1],'poster':JSON.stringify(this.upData['poster']),
         'ppt':JSON.stringify(this.upData['ppt']),'doc':JSON.stringify(this.upData['doc']),'code':JSON.stringify(this.upData['code'])}
 
-        $.ajax({
-            url: '/systemManage/projectManage/projectShow/modifyProject',
-            type: 'POST',
-            data: sendData,
-            datatype: 'json'
-        }).done(function (rcvMessage) {
-            console.log(rcvMessage)
-            // window.location.reload();
-        })
+        // $.ajax({
+        //     url: '/systemManage/projectManage/projectShow/modifyProject',
+        //     type: 'POST',
+        //     data: sendData,
+        //     datatype: 'json'
+        // }).done(function (rcvMessage) {
+        //     console.log(rcvMessage)
+        //     // window.location.reload();
+        // })
     }
 }
 const imgSrc={'poster':'../../images/poster.png','ppt':'../../images/microsoft-powerpoint.png',
               'doc':'../../images/microsoft-word.png','code':'../../images/compressed.png',download:"../../images/download_icon.png"}
 
-const info={'certification':true,'title':'生活助理','description':'他會幫助你的生活大小事。\n與他聊天，生活解悶\n生活記帳，自動分類，無流水帳\n\n\n\n你好',
-            'poster':'專題成果報告書_行車安全警示系統.pdf','ppt':'聊天機器人(上).pptx',
-            'doc':'專題成果報告書_NeoHand2.docx','code':'GA_A1065506.zip'}
+// const info={'certification':true,'title':'生活助理','description':'他會幫助你的生活大小事。\n與他聊天，生活解悶\n生活記帳，自動分類，無流水帳\n\n\n\n你好',
+//             'poster':'專題成果報告書_行車安全警示系統.pdf','ppt':'聊天機器人(上).pptx',
+//             'doc':'專題成果報告書_NeoHand2.docx','code':'GA_A1065506.zip'}
 
-/*const info={'certification':true,'title':null,'description':null,
-            'poster':null,'ppt':null,
-            'doc':null,'code':null}*/
-const ps=new ProjectShow(info,imgSrc)
-
-
-// for test
+// const info={'certification':true,'title':null,'description':null,
+//             'poster':null,'ppt':null,
+//             'doc':null,'code':null}
+let info
 $.ajax({
-    url: '/systemManage/projectManage/projectShow/download',
-    type: 'POST',
-    data: '',
-    datatype: 'json'
-}).done(function (rcvMessage) {
-    console.log(rcvMessage)
-})
+        url: '/systemManage/projectManage/projectShow/getData',
+        type: 'POST',
+        data: null,
+        datatype: 'json'
+    }).done(function (rcvMessage) {
+        info=rcvMessage
+    })
+
+const ps=new ProjectShow(info,imgSrc)
